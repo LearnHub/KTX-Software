ff4d3100848548de8ceabd41ec4864d5cd96bd88
MarkCallow <github@callow.im>
2021-09-23 17:07:29 +0900

    Use older style vcpkg command.

--- ci_scripts/install_windows.bat
+++ ci_scripts/install_windows.bat
@@ -1,10 +1,12 @@
+@echo OFF
 REM Copyright 2021 The Khronos Group Inc.
 REM SPDX-License-Identifier: Apache-2.0
-echo OFF
 
 if NOT EXIST C:\Tools\vcpkg\packages\openimageio* (
     echo "Installing OpenImageIO package and its dependencies."
-    vcpkg install --triplet=x64-windows openimageio
+    REM --triplet not supported in VS2015 environment so use older style.
+    REM vcpkg install --triplet=x64-windows openimageio
+    vcpkg install openimageio:x64-windows
 ) else (
     echo "Upgrading OpenImageIO package and its dependencies."
     vcpkg upgrade
